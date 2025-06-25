4cdc5762ac9202ca6b1b9094c6646373f7f54b49
jsmmr2 <justints890@gmail.com>
2025-06-24 22:30:15 -0500

    I'm slowly but surely figuring this out...

--- app/src/main/java/org/example/ObjectRecognition.java
+++ app/src/main/java/org/example/ObjectRecognition.java
@@ -1,15 +1,25 @@
 package org.example;
 
-import org.tensorflow.SavedModelBundle;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
-public class ObjectRecognition { 
-    private SavedModelBundle model;
-
-    public ObjectRecognition(String path) {
-        model = SavedModelBundle.load(path, "serve");
-    }
-
+public class ObjectRecognition {
     public static void main(String[] args) {
-        ObjectRecognition recognition = new ObjectRecognition("C:/path/to/model");
+        // Gets image from file
+        Mat image = Imgcodecs.imread("path/to/image");
+        // Creating what will be result image
+        Mat resizedImage = new Mat();
+        // Size to resize the image to
+        Size imageSize = new Size(224, 224);
+        // Actually resizing the image to what we want
+        Imgproc.resize(image, resizedImage, imageSize);
+        // Yippee, another mat in the conversion process
+        Mat floatMat = new Mat();
+        // Converts the 224x224 image to a 32 bit float mat (I think) (and also scales values from 0-255 to 0-1)
+        resizedImage.convertTo(floatMat, CvType.CV_32F, 1.0/255.0);
+        
     }
 }
